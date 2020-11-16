e08469d4ae3c3246d9573f5cd292b5a40caeb019
Robert Radford <RobertR928@gmail.com>
2020-11-16 18:01:11 -0500

    complete code challenge

--- code-challenges/linked_list/test_linked_list.py
+++ code-challenges/linked_list/test_linked_list.py
@@ -43,3 +43,71 @@ def test_return_all_values():
 	initialList.insert(7)
 	initialList.insert(8)
 	assert str(initialList) == '8 ->7 ->6 ->5'
+
+def test_append():
+	initialList = LinkedList()
+	initialList.append(5)
+	initialList.append(6)
+	initialList.append(7)
+	initialList.append(8)
+	assert str(initialList) == '5 ->6 ->7 ->8'
+
+def test_insert_before():
+	initialList = LinkedList()
+	initialList.append(5)
+	initialList.append(6)
+	initialList.append(8)
+	initialList.insertBefore(7, 8)
+	assert str(initialList) == '5 ->6 ->7 ->8'
+
+def test_insert_after():
+	initialList = LinkedList()
+	initialList.append(5)
+	initialList.append(6)
+	initialList.append(8)
+	initialList.insertAfter(7, 6)
+	assert str(initialList) == '5 ->6 ->7 ->8'
+
+def test_insert_before():
+	initialList = LinkedList()
+	initialList.append(6)
+	initialList.append(7)
+	initialList.append(8)
+	initialList.insertBefore(5, 6)
+	assert str(initialList) == '5 ->6 ->7 ->8'
+
+def test_insert_after():
+	initialList = LinkedList()
+	initialList.append(5)
+	initialList.append(6)
+	initialList.append(7)
+	initialList.insertAfter(8, 7)
+	assert str(initialList) == '5 ->6 ->7 ->8'
+
+def test_insert_delete_middle():
+	initialList = LinkedList()
+	initialList.append(5)
+	initialList.append(6)
+	initialList.append(7)
+	initialList.insertAfter(8, 7)
+	initialList.delete(6)
+	assert str(initialList) == '5 ->7 ->8'
+
+def test_insert_delete_first():
+	initialList = LinkedList()
+	initialList.append(5)
+	initialList.append(6)
+	initialList.append(7)
+	initialList.insertAfter(8, 7)
+	initialList.delete(5)
+	assert str(initialList) == '6 ->7 ->8'
+
+def test_insert_delete_last():
+	initialList = LinkedList()
+	initialList.append(5)
+	initialList.append(6)
+	initialList.append(7)
+	initialList.insertAfter(8, 7)
+	initialList.delete(8)
+	assert str(initialList) == '5 ->6 ->7'
+
