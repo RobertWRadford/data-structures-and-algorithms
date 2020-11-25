c75470c1acfb99dd4c0c5c0a875ac36233d8037c
Robert Radford <RobertR928@gmail.com>
2020-11-24 20:22:01 -0500

    final challenge state

--- code-challenges/fifo_animal_shelter/test_fifo_animal_shelter.py
+++ code-challenges/fifo_animal_shelter/test_fifo_animal_shelter.py
@@ -1,16 +1,8 @@
 import pytest
-from fifo_animal_shelter import Animal, Waiting_List, Animal_Shelter
+from fifo_animal_shelter import Animal, Waiting_List, Animal_Shelter, InvalidOperationError
 
-def test_Animal_Shelter_sequence():
-	animal_shelter = Animal_Shelter()
-	animal_shelter.welcome_in('dog')
-	animal_shelter.welcome_in('cat')
-	animal_shelter.welcome_in('cat')
-	animal_shelter.welcome_in('cat')
-	animal_shelter.welcome_in('dog')
-	animal_shelter.welcome_in('dog')
-	animal_shelter.welcome_in('cat')
-	assert str(animal_shelter) == 'dog ->cat ->cat ->cat ->dog ->dog ->cat'
+def test_Animal_Shelter_sequence(default_shelter):
+	assert str(default_shelter) == 'dog ->cat ->cat ->cat ->dog ->dog ->cat'
 
 def test_Animal_Shelter_no_dogs():
 	animal_shelter = Animal_Shelter()
@@ -20,41 +12,20 @@ def test_Animal_Shelter_no_dogs():
 	animal_shelter.welcome_in('cat')
 	assert animal_shelter.adoption_out('dog') == 'There are no dogs left.'
 
-def test_Animal_Shelter_adopt_first():
-	animal_shelter = Animal_Shelter()
-	animal_shelter.welcome_in('dog')
-	animal_shelter.welcome_in('cat')
-	animal_shelter.welcome_in('cat')
-	animal_shelter.welcome_in('cat')
-	animal_shelter.welcome_in('dog')
-	animal_shelter.welcome_in('dog')
-	animal_shelter.welcome_in('cat')
-	animal_shelter.adoption_out('dog')
-	assert str(animal_shelter) == 'cat ->cat ->cat ->dog ->dog ->cat'
+def test_Animal_Adoption_adoption(default_shelter):
+	assert default_shelter.adoption_out('dog') == 'dog'
 
-def test_Animal_Shelter_adopt_not_first():
-	animal_shelter = Animal_Shelter()
-	animal_shelter.welcome_in('dog')
-	animal_shelter.welcome_in('cat')
-	animal_shelter.welcome_in('cat')
-	animal_shelter.welcome_in('cat')
-	animal_shelter.welcome_in('dog')
-	animal_shelter.welcome_in('dog')
-	animal_shelter.welcome_in('cat')
-	animal_shelter.adoption_out('cat')
-	assert str(animal_shelter) == 'dog ->cat ->cat ->dog ->dog ->cat'
+def test_Animal_Shelter_adopt_first(default_shelter):
+	default_shelter.adoption_out('dog')
+	assert str(default_shelter) == 'cat ->cat ->cat ->dog ->dog ->cat'
 
-def test_Animal_Shelter_adopt_no_preference():
-	animal_shelter = Animal_Shelter()
-	animal_shelter.welcome_in('dog')
-	animal_shelter.welcome_in('cat')
-	animal_shelter.welcome_in('cat')
-	animal_shelter.welcome_in('cat')
-	animal_shelter.welcome_in('dog')
-	animal_shelter.welcome_in('dog')
-	animal_shelter.welcome_in('cat')
-	animal_shelter.adoption_out()
-	assert str(animal_shelter) == 'cat ->cat ->dog ->dog ->cat'
+def test_Animal_Shelter_adopt_not_first(default_shelter):
+	default_shelter.adoption_out('cat')
+	assert str(default_shelter) == 'dog ->cat ->cat ->dog ->dog ->cat'
+
+def test_Animal_Shelter_adopt_no_preference(default_shelter):
+	default_shelter.adoption_out()
+	assert str(default_shelter) == 'cat ->cat ->cat ->dog ->dog ->cat'
 
 def test_Animal_Shelter_wrong_animal_given():
 	animal_shelter = Animal_Shelter()
@@ -70,3 +41,15 @@ def test_Animal_Shelter_nothing_to_adopt():
 	animal_shelter = Animal_Shelter()
 	with pytest.raises(InvalidOperationError):
 		animal_shelter.adoption_out('dog')
+
+@pytest.fixture
+def default_shelter():
+	animal_shelter = Animal_Shelter()
+	animal_shelter.welcome_in('dog')
+	animal_shelter.welcome_in('cat')
+	animal_shelter.welcome_in('cat')
+	animal_shelter.welcome_in('cat')
+	animal_shelter.welcome_in('dog')
+	animal_shelter.welcome_in('dog')
+	animal_shelter.welcome_in('cat')
+	return animal_shelter
