76590e33a106e9fc3b0eb4ac895ff6ed7097cd9b
Robert Radford <RobertR928@gmail.com>
2020-11-30 23:33:27 -0500

    init tree

---
+++ code-challenges/tree/tree.py
@@ -0,0 +1,80 @@
+class Node:
+
+	def __init__(self, value=None, left=None, right=None):
+		self.value = value
+		self.right = right
+		self.left = left
+
+class BinaryTree:
+
+	def __init__(self, head=None):
+		self.head = head
+
+	def add(self, value=None, root=None):
+		if value is None:
+			raise TypeError
+		if not root:
+			if not self.head:
+				self.head = Node(value)
+			else:
+				root = self.head
+		if value < root.value:
+			if not root.left:
+				root.left = Node(value)
+			else:
+				self.add(value, root.left)
+		else:
+			if not root.right:
+				root.right = Node(value)
+			else:
+				self.add(value, root.right)
+
+
+	def contains(self, value=None, root=None):
+		if value is None:
+			raise TypeError
+		if not root:
+			if not self.head:
+				return False
+			else:
+				root = self.head
+		if value == root.value:
+			return True
+		if value < root.value:
+			self.contains(value, root.left)
+		else:
+			self.contains(value, root.right)
+
+	def preOrder(self, root=None, contents = []):
+		if not root:
+			root = self.head
+		if root:
+			contents.append(root.value)
+			if root.left:
+				self.preOrder(root.left, contents)
+			if root.right:
+				self.preOrder(root.right, contents)
+		return contents
+
+
+	def inOrder(self, root=None, contents = []):
+		if not root:
+			root = self.head
+		if root:
+			if root.left:
+				self.inOrder(root.left, contents)
+			contents.append(root.value)
+			if root.right:
+				self.inOrder(root.right, contents)
+		return contents
+
+	def postOrder(self, root=None, contents = []):
+		if not root:
+			root = self.head
+		if root:
+			if root.left:
+				self.postOrder(root.left, contents)
+			if root.right:
+				self.postOrder(root.right, contents)
+			contents.append(root.value)
+		return contents
