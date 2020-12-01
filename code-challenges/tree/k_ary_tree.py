55cfb3b8b902ae0b0c6c166d49bc542df7f2192d
Robert Radford <RobertR928@gmail.com>
2020-12-01 17:22:09 -0500

    fixed k ary

--- code-challenges/tree/k_ary_tree.py
+++ code-challenges/tree/k_ary_tree.py
@@ -9,29 +9,6 @@ class karyTree:
 	def __init__(self, k, values=[]):
 		if not values or not k:
 			raise TypeError
-		current_tier = []
-		next_tier = []
-		self.head = Node(values[0])
-		current_tier.append(self.head)
-		values.pop(0)
-		while values:
-			for node in current_tier:
-				root = node
-				# print(root.value)
-				for i in range(k):
-					next_node = Node(values[0])
-					root.children.append(next_node)
-					next_tier.append(next_node)
-					values.pop(0)
-			# print([root.children[i].value for i, node in enumerate(root.children)])
-			current_tier.clear()
-			current_tier.extend(next_tier)
-			# print([current_tier[i].value for i in range(len(current_tier))])
-			next_tier.clear()
-
-	def __init__(self, k, values=[]):
-		if not values or not k:
-			raise TypeError
 		self.head = Node(values[0])
 		values.pop(0)
 		current_tier = [self.head]
