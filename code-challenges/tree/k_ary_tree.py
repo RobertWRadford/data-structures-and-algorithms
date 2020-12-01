3a35413fd0c4d9fe0aa50540e0a9a15194650fa3
Robert Radford <RobertR928@gmail.com>
2020-12-01 17:20:14 -0500

    fixed kary sloppiness

--- code-challenges/tree/k_ary_tree.py
+++ code-challenges/tree/k_ary_tree.py
@@ -29,6 +29,21 @@ class karyTree:
 			# print([current_tier[i].value for i in range(len(current_tier))])
 			next_tier.clear()
 
+	def __init__(self, k, values=[]):
+		if not values or not k:
+			raise TypeError
+		self.head = Node(values[0])
+		values.pop(0)
+		current_tier = [self.head]
+		while values:
+			root = current_tier[0]
+			current_tier.pop(0)
+			for i in range(k):
+				next_node = Node(values[0])
+				root.children.append(next_node)
+				current_tier.append(next_node)
+				values.pop(0)
+
 if __name__ == '__main__':
 	kTree = karyTree(3, [i for i in range(1, 41)])
 	print([kTree.head.children[0].children[i].value for i, values in enumerate(kTree.head.children[0].children)])
